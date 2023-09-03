660d4318eb9e4b28c70304439330cbb254d67747
nivethjunnithan <nivethunnnithan@gmail.com>
2023-09-03 21:31:49 +0530

    updated usePromise hook

--- hooks/usePromise.jsx
+++ hooks/usePromise.jsx
@@ -1,14 +1,10 @@
-import React, { useRef, useEffect } from 'react'
-
-
 /**
  * @function usePromise
- * @description React Hook to wrap  * 
- * @param {Function} effect
- * @param {Array<any>} dependencies
+ * @description React Hook to wrap a promise with cleanup logic
+ * @param {Function} callback
  * @returns {Function} cleanUpFunction()
  * @author Sarat Chandra Ejjapureddi
  */
 export default function usePromise(callback) {
-    
+  // Your hook implementation here
 }
