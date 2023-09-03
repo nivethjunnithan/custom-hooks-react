8d7580f47a31af362e8ba8e03a7b9aae517d9f45
nivethjunnithan <nivethunnnithan@gmail.com>
2023-09-03 21:32:09 +0530

    updated useSelectedList hook

--- hooks/useSelectedList.jsx
+++ hooks/useSelectedList.jsx
@@ -1,29 +1,24 @@
-import React, { useState, useEffect } from 'react'
+import { useState } from "react";
 
 /**
  * @function useSelectedList
  * @description React Hook to use List where we can select or unselect list of items
  * @param {any} value
- * @returns {any} 
+ * @returns {any}
  * @author Sarat Chandra Ejjapureddi
  */
 export default function useSelectedList(defaultList = [], uniqueValue) {
+  //-------------------IN Progress
 
+  const [list, setList] = useState(defaultList);
+  const [uniqueValue, setUniqueValue] = useState(uniqueValue);
+  const [selectedList, setSelectedList] = useState(uniqueValue);
 
-    //-------------------IN Progress 
-
-    const [list, setList] = useState(defaultList)
-    const [uniqueValue, setUniqueValue] = useState(uniqueValue)
-    const [selectedList, setSelectedList] = useState(uniqueValue)
-
-
-    
-
-    const handleInputValue = (e) => {
-        if (e) {
-            setValue(e.target.value)
-        }
+  const handleInputValue = (e) => {
+    if (e) {
+      setValue(e.target.value);
     }
+  };
 
-    return [list]
+  return [list];
 }
